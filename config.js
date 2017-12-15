15ceea98f9f8b61b1289eeff6cf5f118a76f0fe4
jeanaquino <jeanaquino@localhost.localdomain>
2017-12-15 18:03:28 -0200

    Configurações Iniciais

---
+++ config.js
@@ -0,0 +1,12 @@
+var restify = require('restify');
+
+module.exports = () => {
+    var server = restify.createServer();
+    server.get('/', respond);
+
+    function respond(req, res, next) {
+        res.charSet('utf-8');
+        res.send(200, {mensagem : "A aplicação AuthorizationToken está OnLine"})
+    }
+    return server;
+}
